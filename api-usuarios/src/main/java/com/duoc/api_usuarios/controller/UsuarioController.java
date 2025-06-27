1ecf4921792dd24b78bbf058aa5ed86dd3720894
Jaime Hermosilla <jaimelearn1@gmail.com>
2025-06-27 00:12:42 -0400

    El resto de swagger, repetir en los demás microservicios

--- api-usuarios/src/main/java/com/duoc/api_usuarios/controller/UsuarioController.java
+++ api-usuarios/src/main/java/com/duoc/api_usuarios/controller/UsuarioController.java
@@ -16,14 +16,20 @@ import com.duoc.api_usuarios.model.entity.Usuario;
 import com.duoc.api_usuarios.model.request.UsuarioCreate;
 import com.duoc.api_usuarios.model.request.UsuarioUpdate;
 import com.duoc.api_usuarios.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 /*------------------------------------------*/
 
+
 @RestController
 @RequestMapping("/users")
 public class UsuarioController {
-    
+
     // Atributos
     @Autowired
     public UsuarioService usuarioServ;
@@ -40,11 +46,33 @@ public class UsuarioController {
         return usuarioServ.obtenerActivos();
     }
 
+
+    @Operation(summary = "Crear y registrar a un usuario")
+    // Permite varias respuestas del endpoint
+    @ApiResponses(value = { 
+        // Que pasa si la respuestas es existosa
+        @ApiResponse(responseCode = "200", description = "El usuario se a creado exitosamente!",
+            content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "1"))),
+        // Que pasa si la respuestas no es existosa
+        @ApiResponse(responseCode = "400", description = "Parámetros inválidos")
+    })
+
     // Registrar un usuario
     @PostMapping("/auth/register")
     public Usuario registrar(@Valid @RequestBody UsuarioCreate datosCrear) {
         return usuarioServ.agregar(datosCrear);
     }
+
+
+    @Operation(summary = "Eliminar a un usuario por su ID")
+    // Permite varias respuestas del endpoint
+    @ApiResponses(value = { 
+        // Que pasa si la respuestas es existosa
+        @ApiResponse(responseCode = "200", description = "El usuario a sido eliminado/deshabilitado exitosamente!",
+            content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "1"))),
+        // Que pasa si la respuestas no es existosa
+        @ApiResponse(responseCode = "400", description = "Parámetros inválidos")
+    })
 
     // Eliminar a un usuario
     @PutMapping("/remove/{id}")
@@ -53,8 +81,19 @@ public class UsuarioController {
         return "Usuario eliminado-desactivado!";
     }
 
+
+    @Operation(summary = "Modificar a un usuario por su ID")
+    // Permite varias respuestas del endpoint
+    @ApiResponses(value = { 
+        // Que pasa si la respuestas es existosa
+        @ApiResponse(responseCode = "200", description = "El usuario a sido modificado/actualizado exitosamente!",
+            content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "1"))),
+        // Que pasa si la respuestas no es existosa
+        @ApiResponse(responseCode = "400", description = "Parámetros inválidos")
+    })
+
     // Modificar un usuario
-    @PutMapping("/{id}")
+    @PutMapping("/modify/{id}")
     public String modificar(@Valid @RequestBody UsuarioUpdate datosModificar) {
         usuarioServ.modificar(datosModificar);
         return "Usuario modificado!";
